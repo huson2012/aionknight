fd69e99ec0f0c2e82ca7cb8ff6914081f3c663c2
flash02142010 <flash02142010@gmail.com>
2011-10-16 17:08:18 +0000

    Добавлен Квест: Память Нархи(18405) Добавлены NPC: Отрис Гирр Дани Филлиа Элрин Джионни Фабиан Уминос Лиа Антигона Исправлены пути некоторых NPC Поправленны ошибки при компиляции на Linux P.S. Незабываем заливать в базу:  /trunk/gameserver/sql/update/special_spawns.sql Без него спавн в арене не запустится

---
+++ gameserver/data/scripts/system/handlers/quest/esoterrace/_18405MemoriesintheCornerofHisMind.java
@@ -0,0 +1,72 @@
+/*
+ * This file is part of Aion-Knight Dev. Team <aion-knight.ru>.
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package quest.esoterrace;
+
+import org.openaion.gameserver.model.gameobjects.player.Player;
+import org.openaion.gameserver.quest.handlers.QuestHandler;
+import org.openaion.gameserver.quest.model.QuestCookie;
+import org.openaion.gameserver.quest.model.QuestState;
+import org.openaion.gameserver.quest.model.QuestStatus;
+
+/**
+ * Author: Dimoalex
+ * Modified: Frost
+ */
+
+public class _18405MemoriesintheCornerofHisMind extends QuestHandler {
+    private final static int questId = 18405;
+
+    public _18405MemoriesintheCornerofHisMind() {
+        super(questId);
+    }
+
+    @Override
+    public void register() {
+        qe.setNpcQuestData(799553).addOnTalkEvent(questId);
+		qe.setNpcQuestData(799552).addOnTalkEvent(questId);
+    }
+
+    @Override
+    public boolean onDialogEvent(QuestCookie env) {
+        Player player = env.getPlayer();
+
+        if (env.getTargetId() == 0)
+            return defaultQuestStartItem(env);
+
+        QuestState qs = player.getQuestStateList().getQuestState(questId);
+        if (qs == null)
+            return false;
+
+        int var = qs.getQuestVarById(0);
+
+        if (qs.getStatus() == QuestStatus.START) {
+            if (env.getTargetId() == 799553) {
+                switch (env.getDialogId()) {
+                    case 26:
+                        if (var == 1)
+                    return sendQuestDialog(env, 1011);
+
+					case 1009:
+                        defaultQuestRemoveItem(env, 188051351, 1);
+                    return defaultCloseDialog(env, 0, 1, true, true);
+                }
+            }
+        }
+        return defaultQuestRewardDialog(env, 799552, 2375);
+    }
+}
